954bd3a6dfa8ed4a5b57187ff62769ecc1323159
Milos Dimic <dimic.milos@icloud.com>
2019-12-14 18:18:33 +0100

    Initial Commit

---
+++ Frameworks/Frameworks.h
@@ -0,0 +1,19 @@
+//
+//  Frameworks.h
+//  Frameworks
+//
+//  Created by Dimic Milos on 12/14/19.
+//  Copyright © 2019 Dimic Milos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Frameworks.
+FOUNDATION_EXPORT double FrameworksVersionNumber;
+
+//! Project version string for Frameworks.
+FOUNDATION_EXPORT const unsigned char FrameworksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Frameworks/PublicHeader.h>
+
+
